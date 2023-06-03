d0ced7966d75f8f7badf2267fa86b2f3da454ba9
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-06-03 21:50:54 +0700

    Update data.py

--- Ubot/core/data.py
+++ Ubot/core/data.py
@@ -3,6 +3,6 @@ from Ubot import cmds
 class Data:
 
     text_help_menu = (
-        f"**Help Menu**\n**• Prefixes** : `. ! ?`"
+        f"**𝘿𝙕-𝙐𝙎𝙀𝙍𝘽𝙊𝙏🤖**\n━━━━━━━━━━━━━━━━━━\nᴛʏᴘᴇ : ᴘʀᴇᴍɪᴜᴍ💎\nᴀᴄᴛɪᴠᴇ : 𝟷 ʙᴜʟᴀɴ\nᴏᴡɴᴇʀ : [🄼🅂 𝗗🆉𝗨𝗟𝚀𝐔𝐑𝐍Λ𝐈𝐍](https://t.me/MSDZULQRNN)\nᴜᴘᴅᴀᴛᴇs : @MSPR0JECT\n━━━━━━━━━━━━━━━━━━\nᴋʟɪᴋ ʙᴜᴛᴛᴏɴ ᴅɪʙᴀᴡᴀʜ ᴜɴᴛᴜᴋ ᴍᴇʟɪʜᴀᴛ ᴍᴏᴅᴜʟ !"
     )
     reopen = [[InlineKeyboardButton("Open", callback_data="reopen")]]
